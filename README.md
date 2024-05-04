8e968c981640f00e3dd076faf5502322c64eea04
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-05-04 11:34:57 +0530

    added comments in main.py & updated readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # fastAPI
+FastAPI is an open-source web framework that uses Python to create APIs
+
+## Requirements
+
+● Using a Python web framework FastAPI to build the API.
+● Implement endpoints for the following functionalities:
+ - GET /products: Retrieve a list of all products from a database.
+ - GET /products/{id}: Retrieve details of a specific product by its ID.
+ - POST /products: Create a new product with details like title, description,price, etc. (data should be received in JSON format).
+ - PUT /products/{id}: Update an existing product based on its ID.
+ - DELETE /products/{id}: Delete a product by its ID.
+
+
+## Create new python env 
+ - python3 -m venv env
+ - source env/bin/activate  (if failed)
+ - pip install fastapi uvicorn sqlalchemy
+
+## To launch fastAPI
+ - uvicorn main:app --reload
--- main.py
+++ main.py
@@ -12,19 +12,18 @@ app = FastAPI()
 # Create tables
 models.Base.metadata.create_all(bind=engine)
 
-
+# Pydantic models
 class read_products( BaseModel):
     title: str
     description: str
     price: int
 
 
-
 class read_product(BaseModel):
     id: int
     title: str
     description: str
-    price: float  # Assuming price is a float, adjust as needed
+    price: float
 
 
 class ProductCreate(BaseModel):
@@ -48,6 +47,8 @@ def get_db():
 
 db_dependency = Annotated[Session,Depends(get_db)]
 
+
+#To Retrieve a list of all products from a database.
 @app.get("/products/", status_code=status.HTTP_200_OK)
 async def read_products(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     products = db.query(models.Product).offset(skip).limit(limit).all()
@@ -55,6 +56,8 @@ async def read_products(skip: int = 0, limit: int = 10, db: Session = Depends(ge
         raise HTTPException(status_code=404,detail='No Products available')
     return products
 
+
+#To Retrieve details of a specific product by its ID.
 @app.get("/products/{product_id}", status_code=status.HTTP_200_OK)
 async def read_product(product_id: int, db: Session = Depends(get_db)):
     db_product_id = db.query(models.Product).filter(models.Product.id == product_id).first()
@@ -62,6 +65,8 @@ async def read_product(product_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Product not found")
     return db_product_id
 
+
+#To Create a new product
 @app.post("/products/", status_code=status.HTTP_201_CREATED)
 async def create_product(product: ProductCreate, db: Session = Depends(get_db)):
     db_product = models.Product(**product.dict())
@@ -70,6 +75,7 @@ async def create_product(product: ProductCreate, db: Session = Depends(get_db)):
     db.refresh(db_product)
     return db_product
 
+#To Update an existing product based on its ID.
 @app.put("/products/{product_id}", status_code=status.HTTP_202_ACCEPTED)
 async def update_product(product_id: int, product: ProductUpdate, db: Session = Depends(get_db)):
     db_product_update = db.query(models.Product).filter(models.Product.id == product_id).first()
@@ -81,7 +87,9 @@ async def update_product(product_id: int, product: ProductUpdate, db: Session = 
     db.refresh(db_product_update)
     return db_product_update
 
-@app.delete("/products/{product_id}")
+
+#To Delete a product by its ID.
+@app.delete("/products/{product_id}", status_code=status.HTTP_200_OK)
 async def delete_product(product_id: int, db: Session = Depends(get_db)):
     db_product = db.query(models.Product).filter(models.Product.id == product_id).first()
     if db_product is None:
